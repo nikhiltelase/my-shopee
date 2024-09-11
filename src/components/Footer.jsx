93e02b3459e3f0065b1bd28fd658e503109779e5
nikhil <nikhiltelase@gmail.com>
2024-09-11 12:23:04 +0530

    add apk file

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
           <h3 className="text-2xl mb-3 sm:text-3xl">Download our app</h3>
           <p className="text-sm sm:text-base">Shop smarter with exclusive offers and seamless shopping experience.</p>
           <div className="flex flex-col items-center px-16 mt-3 space-y-3 sm:flex-row sm:space-y-0 sm:space-x-4">
-            <div className="flex items-center border w-full rounded-lg px-4 py-2 cursor-pointer">
+            <a href="/my-shopee.apk" download className="flex items-center border w-full rounded-lg px-4 py-2 cursor-pointer">
               <img
                 src="https://cdn-icons-png.flaticon.com/512/888/888857.png"
                 className="w-6 sm:w-7"
@@ -18,8 +18,8 @@ const Footer = () => {
                 <p className="text-xs text-gray-200">Download on</p>
                 <p className="text-sm sm:text-base">Google Play Store</p>
               </div>
-            </div>
-            <div className="flex items-center border w-full rounded-lg px-4 py-2 cursor-pointer">
+            </a>
+            <a href="/my-shopee.apk" download className="flex items-center border w-full rounded-lg px-4 py-2 cursor-pointer">
               <img
                 src="https://cdn-icons-png.flaticon.com/512/888/888841.png"
                 className="w-6 sm:w-7"
@@ -29,7 +29,7 @@ const Footer = () => {
                 <p className="text-xs text-gray-200">Download on</p>
                 <p className="text-sm sm:text-base">Apple Store</p>
               </div>
-            </div>
+            </a>
           </div>
         </div>
         <div className="mt-3 flex flex-col items-center space-y-4 text-sm text-gray-400 sm:flex-row sm:justify-between sm:space-y-0">
